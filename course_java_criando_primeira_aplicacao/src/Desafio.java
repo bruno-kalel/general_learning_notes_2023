7e3e3b84616faa4ea2fb4946747b19d4a9c445ce
Bruno Kalel <104171372+bruno-kalel@users.noreply.github.com>
2023-11-11 13:09:46 -0300

    Tame Impala - Let It Happen

--- course_java_criando_primeira_aplicacao/src/Desafio.java
+++ course_java_criando_primeira_aplicacao/src/Desafio.java
@@ -5,7 +5,7 @@ public class Desafio
     public static void main(String[] args)
     {
         Scanner leitura = new Scanner(System.in);
-        String nome = "Nome do cliente";
+        String nome = "Bruno";
         String tipo = "Corrente";
         double saldo = 2500;
 
@@ -62,8 +62,16 @@ public class Desafio
                 case 3:
                     System.out.println(3);
                     System.out.println("Insira o valor a ser sacado:");
-                    saldo -= leitura.nextDouble();
-                    System.out.printf("Novo valor de saldo: %.2f\n", saldo);
+                    if (leitura.nextDouble() > saldo)
+                    {
+                        System.out.println("Saldo insuficiente para esta operação");
+                    }
+                    else
+                    {
+                        saldo -= leitura.nextDouble();
+                        System.out.printf("Novo valor de saldo: %.2f\n", saldo);
+                    }
+
                     break;
 
                 case 4:
